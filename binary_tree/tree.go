b91654177fbdda4cb70139b42460d6cc6e029340
04637 <04637@163.com>
2020-11-02 14:46:42 +0800

    前中后序遍历

--- binary_tree/tree.go
+++ binary_tree/tree.go
@@ -18,6 +18,26 @@ func PreorderTraversal(node *TreeNode) {
 	PreorderTraversal(node.right)
 }
 
+// 中序遍历
+func InorderTraversal(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	InorderTraversal(node.left)
+	node.print()
+	InorderTraversal(node.right)
+}
+
+// 后续遍历
+func PostorderTraversal(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	PostorderTraversal(node.left)
+	PostorderTraversal(node.right)
+	node.print()
+}
+
 func (n TreeNode) print() {
 	fmt.Print(" => ", n.val)
 }
--- binary_tree/tree_test.go
+++ binary_tree/tree_test.go
@@ -1,7 +1,66 @@
 package binary_tree
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestPreorderTraversal(t *testing.T) {
+func TestTraversal(t *testing.T) {
+	tree := generateTree()
+	fmt.Println("preorder: ")
+	PreorderTraversal(tree)
+	fmt.Println("\n--------------------------------")
+	fmt.Println("inorder: ")
+	InorderTraversal(tree)
+	fmt.Println("\n--------------------------------")
+	fmt.Println("postorder: ")
+	PostorderTraversal(tree)
+	fmt.Println("\n--------------------------------")
+	fmt.Println()
+}
 
+func generateTree() *TreeNode {
+	root := &TreeNode{}
+	root.val = 5
+	root.left = &TreeNode{
+		val: 3,
+	}
+	root.right = &TreeNode{
+		val: 8,
+	}
+	// root 3
+	curRoot := root.left
+	curRoot.left = &TreeNode{
+		val: 1,
+	}
+	curRoot.right = &TreeNode{
+		val: 4,
+	}
+	// root 1
+	curRoot = curRoot.left
+	curRoot.left = &TreeNode{
+		val: 0,
+	}
+	curRoot.right = &TreeNode{
+		val: 2,
+	}
+	// root 8
+	curRoot = root.right
+	curRoot.left = &TreeNode{
+		val: 6,
+	}
+	curRoot.right = &TreeNode{
+		val: 10,
+	}
+	// root 6
+	curLeft := curRoot.left
+	curLeft.right = &TreeNode{
+		val: 7,
+	}
+	// root 10
+	curRight := curRoot.right
+	curRight.left = &TreeNode{
+		val: 9,
+	}
+	return root
 }
